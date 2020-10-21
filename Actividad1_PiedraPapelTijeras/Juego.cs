60d50f549bc568f3d68d79ec1d4d4d3e106b9d11
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-10-20 19:23:33 -0500

    Avance Piedra, Papel o Tijeras

--- Actividad1_PiedraPapelTijeras/Juego.cs
+++ Actividad1_PiedraPapelTijeras/Juego.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
+using System.Reflection.Emit;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -12,9 +15,9 @@ using System.Windows.Threading;
 namespace Actividad1_PiedraPapelTijeras
 {
     public enum Opcion { PIEDRA, PAPEL, TIJERAS }
-    public class Juego: INotifyPropertyChanged
+    public class Juego : INotifyPropertyChanged
     {
-        public string Jugador1 { get; set; }
+        public string Jugador1 { get; set; } = "Jugador";
         public string Jugador2 { get; set; }
 
         public string PuntosJugador1 { get; set; }
@@ -23,7 +26,10 @@ namespace Actividad1_PiedraPapelTijeras
         public string SeleccionJugador1 { get; set; }
         public string SeleccionJugador2 { get; set; }
         public string Mensaje { get; set; }
+        public string IP { get; set; }
+        public bool MainWindowVisible { get; set; } = true;
         public ICommand SeleccionarOpcionCommand { get; set; }
+        public ICommand IniciarCommand { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -33,17 +39,65 @@ namespace Actividad1_PiedraPapelTijeras
         public Juego()
         {
             currentDispatcher = Dispatcher.CurrentDispatcher;
+            IniciarCommand = new RelayCommand<bool>(IniciarPartida);
         }
 
-        public void CrearPartida(string nombre)
+        private void Lobby_Closing(object sender, CancelEventArgs e)
+        {
+            MainWindowVisible = true;
+            Actualizar();
+            if (servidor != null)
+            {
+                servidor.Stop();
+                servidor = null;
+            }
+        }
+
+        Lobby lobby;
+        private async void IniciarPartida(bool tipoPartida)
+        {
+            try
+            {
+                MainWindowVisible = false;
+                lobby = new Lobby();
+                lobby.Closing += Lobby_Closing;
+                lobby.DataContext = this;
+                lobby.Show();
+                Actualizar();
+                if (tipoPartida == true)
+                {
+                    CrearPartida();
+                }
+                else
+                {
+                    Mensaje = "Intentando conectar con el servidor en " + IP;
+                    Actualizar("Mensaje");
+                    await ConectarPartida();
+                }
+            }
+            catch (Exception ex)
+
+            {
+                Mensaje = ex.Message;
+                Actualizar();
+            }
+        }
+
+        public async Task ConectarPartida()
+        {
+            cliente = new ClientWebSocket();
+            await cliente.ConnectAsync(new Uri($"ws://{IP}:1000/ppt/"), CancellationToken.None);
+
+        }
+
+        public void CrearPartida()
         {
             servidor = new HttpListener();
             servidor.Prefixes.Add("http://*:1000/ppt/");
             servidor.Start();
             servidor.BeginGetContext(OnContext, null);
 
-            Jugador1 = nombre;
-            Mensaje = "Esperando a que se conecte un adversario";
+            Mensaje = "Esperando a que se conecte un adversario...";
             Actualizar();
         }
 
